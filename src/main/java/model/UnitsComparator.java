21e7eaeb1697b9776f52d0ecfd20c06141f368f9
Elena Mosquera <elenamosquera@MacBook-Pro-de-Elena.local>
2020-12-27 21:09:05 +0100

    CODE: método Iterator<Product> MyBagManager.getUnitsIterator()

---
+++ src/main/java/model/UnitsComparator.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Comparator;
+
+public class UnitsComparator implements Comparator<Product> {
+
+	@Override
+	public int compare(Product arg0, Product arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
